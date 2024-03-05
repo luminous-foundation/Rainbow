312692c91723bbf1e1346936e1260e9a47a16571
grimtin10 <grimtin10@gmail.com>
2024-03-04 20:58:28 -0700

    fix project structure

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class Program { 
+    public static void Main(String[] args) {
+        byte[] program = File.ReadAllBytes(args[0]);
+
+        for(int i  = 0; i < program.Length; i++) {
+            Console.WriteLine(program[i]);
+        }
+    }
+}
